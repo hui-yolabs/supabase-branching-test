b9e576d784b494c2de96a76279fdf6bcb514e1f8
Hui Yang <hui@yolabs.io>
2025-06-02 13:53:57 -0700

    save

---
+++ supabase/functions/hello-world/index.ts
@@ -0,0 +1,14 @@
+import { Hono } from 'jsr:@hono/hono';
+
+const app = new Hono();
+
+app.post('/hello-world', async (c) => {
+  const { name } = await c.req.json();
+  return new Response(`Hello ${name}!`)
+});
+
+app.get('/hello-world', (c) => {
+  return new Response('Hello World!')
+});
+
+Deno.serve(app.fetch);
